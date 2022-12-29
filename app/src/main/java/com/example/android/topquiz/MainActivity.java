04a6fa30bdbe3417a24d174cac594133b736749d
badouine <nzbadouine@gmail.com>
2022-12-29 19:23:20 +0000

    run

---
+++ app/src/main/java/com/example/android/topquiz/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.android.topquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview_greeting);
+        mNameEditText = findViewById(R.id.main_edittext_name);
+        mPlayButton = findViewById(R.id.main_button_play);
+        mPlayButton.setEnabled(false);
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mPlayButton.setEnabled(!editable.toString().isEmpty());
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The user just clicked
+            }
+        });
+
+
+    }
+}
