94a6904a3d17e42fccd3bec2f2b2e7bfe4935e38
badouine <nzbadouine@gmail.com>
2022-12-29 21:38:47 +0000

    start intent activity_game_layout

--- app/src/main/java/com/example/android/topquiz/GameActivity.java
+++ app/src/main/java/com/example/android/topquiz/GameActivity.java
@@ -2,13 +2,24 @@ package com.example.android.topquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+    Button mGameButton1;
+    Button mGameButton2;
+    Button mGameButton3;
+    Button mGameButton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mGameButton1 = findViewById(R.id.game_activity_button_1);
+        mGameButton2 = findViewById(R.id.game_activity_button_2);
+        mGameButton3 = findViewById(R.id.game_activity_button_3);
+        mGameButton4 = findViewById(R.id.game_activity_button_4);
     }
 }
--- app/src/main/java/com/example/android/topquiz/MainActivity.java
+++ app/src/main/java/com/example/android/topquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.topquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // The user just clicked
+                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivityIntent);
             }
         });
 
